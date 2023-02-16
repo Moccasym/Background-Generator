f9511e386d3df51185a5b9850642c2593f432355
Felix <57842536+Moccasym@users.noreply.github.com>
2023-02-16 16:53:02 +0100

    Added files

---
+++ script.js
@@ -0,0 +1,31 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector(".randomnator");
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	css.textContent = body.style.background + ";";
+}
+
+function randomColor(){
+	return("#" + Math.floor(Math.random()*16777215).toString(16));
+}
+
+function changeColors(){
+	color1.value = randomColor();
+	console.log(color1.value);
+	color2.value = randomColor();
+	setGradient();
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+document.addEventListener("DOMContentLoaded", setGradient);
+
+button.addEventListener("click", changeColors);
